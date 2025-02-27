7398aaedbe297116b76a4ec238f2cc16d88ee11a
Arnav <2021eo3ar@mitsgwl.ac.in>
2025-02-27 16:46:33 +0530

    chore: update API base URL to production environment

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://data-center-7yhai.ondigitalocean.app';
-const API_BASE_URL = 'http://localhost:8008';
+const API_BASE_URL = 'https://data-center-7yhai.ondigitalocean.app';
+// const API_BASE_URL = 'http://localhost:8008';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
