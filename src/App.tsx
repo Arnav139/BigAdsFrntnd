6e5c38221b924ed7e8650187eb90b34d99b900cb
Arnav <2021eo3ar@mitsgwl.ac.in>
2025-03-19 12:09:58 +0530

    fix: update API documentation link to the new DigitalOcean URL

--- src/App.tsx
+++ src/App.tsx
@@ -165,7 +165,7 @@ function App() {
                         className="w-full"
                         onClick={() =>
                           window.open(
-                            "https://fda8-2409-40d0-1336-a6c3-6cc7-1956-cdd-9f3b.ngrok-free.app/api-docs",
+                            "https://bigads-efwrc.ondigitalocean.app/api-docs",
                             "_blank"
                           )
                         }
